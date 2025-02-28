72f9bd6afbb0a4ec67015f3fa665e014e0d42030
Anderson <andersonecomp091@gmail.com>
2025-02-28 19:22:35 -0300

    Fixing issue with undesired csrf token verification, and finally hitting post client properly

--- leadmanager/routes/api.php
+++ leadmanager/routes/api.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Domains\Client\Controllers\ClientController;
 
+Route::get('/client', [ClientController::class, 'index']);
 Route::post('/client', [ClientController::class, 'create']);
--- leadmanager/routes/web.php
+++ leadmanager/routes/web.php
@@ -3,7 +3,3 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('api')->group(base_path('routes/api.php'));
-
-Route::get('/', function () {
-    return view('welcome');
-});
