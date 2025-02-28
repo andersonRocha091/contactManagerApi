dc97f35a679876efec530a5a71de0db5357eecf6
Anderson <andersonecomp091@gmail.com>
2025-02-28 17:46:00 -0300

    Creating the client request form, and its basic validations

---
+++ leadmanager/app/Domains/Client/Requests/CreateLeadRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Domains\Client\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateClientRequest extends FormRequest {
+
+    public function authorize() {
+        return true;
+    }
+
+    public function rules() {
+        return [
+            'name'=>'required|string|max:255',
+            'email'=>'required|string|max:255',
+            'phone'=> 'nullable|string|max:20',
+            'address'=>'nullable|string|max:255',
+            'city'=>'nullable|string|max:255',
+            'state'=>'nullable|string|max:40',
+            'zip'=>'nullable|string|max:9',
+            'picture'=>'nullable|string',
+            'age' => 'required|integer|min:1'
+        ];
+    }
+}
