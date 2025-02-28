a7a489e7509fa9ce569e127203440c51daeb0077
Anderson <andersonecomp091@gmail.com>
2025-02-28 17:13:39 -0300

    Adding an interface to describe whats necessary for the client repository

---
+++ leadmanager/app/Domains/Client/Repositories/ClientRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Domains\Client\Repositories;
+
+use App\Domains\Client\Domain\Entities\Client;
+
+interface ClientRepositoryInterface {
+
+    public function getAll(array $filters = []);
+    public function getById(int $id);
+    public function create(array $data);
+    public function update(int $id, array $data);
+    public function delete(int $id);
+    
+}
